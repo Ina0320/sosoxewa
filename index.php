c8b0d41e2f882f51d14839cec6d60063ec95e42f
Ina0320 <rizkyinayah0320@gmail.com>
2025-10-04 12:15:23 +0700

    `Fixed image file extensions and updated banner content in index.php`

--- index.php
+++ index.php
@@ -111,7 +111,6 @@
                             <div class="carousel-inner" role="listbox">
                                 <div class="carousel-item active rounded">
                                     <img src="img/hero-img-1.png" class="img-fluid w-100 h-100 bg-secondary rounded" alt="First slide">
-                                    <a href="#" class="btn px-4 py-2 text-white rounded"></a>
                                 </div>
                                 <div class="carousel-item rounded">
                                     <img src="img/hero-img-2.jpg" class="img-fluid w-100 h-100 rounded" alt="Second slide">
